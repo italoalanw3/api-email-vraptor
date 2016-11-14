dbcac76e7c291955b90460348eaf503714eb4101
Ítalo Alan <talo Alan>
2016-11-14 16:58:08 -0300

    Adicionado assunto e metodo generico

--- src/main/java/br/com/logique/controller/EmailController.java
+++ src/main/java/br/com/logique/controller/EmailController.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 
 import javax.inject.Inject;
 
+import org.apache.commons.mail.Email;
 import org.apache.commons.mail.EmailException;
+import org.apache.log4j.Logger;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
@@ -12,6 +14,7 @@ import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.environment.Environment;
 import br.com.caelum.vraptor.view.Results;
+import br.com.logique.controller.email.Assuntos;
 import br.com.logique.controller.email.EmailContainer;
 
 @Controller
@@ -23,29 +26,66 @@ public class EmailController implements Serializable {
 	private final Result result;
 	private EmailContainer emailContainer;
 	private Environment ambiente;
+	Logger log = Logger.getLogger(EmailController.class);
 
 	public EmailController() {
 		this(null, null, null);
 	}
 
 	@Inject
-	public EmailController(Result result, EmailContainer emailContainer, Environment ambiente) {
+	public EmailController(Result result, EmailContainer emailContainer,
+			Environment ambiente) {
 		this.result = result;
 		this.emailContainer = emailContainer;
 		this.ambiente = ambiente;
 	}
-	
-	@Post("/emergencia-falta-gas")
-	public void enviarEmail(String conteudo){
+
+	@Post("/generico")
+	public void enviarEmail(String assunto, String destinatario,
+			String comCopia, String conteudo) {
 		try {
-			this.emailContainer.enviarEmail(this.emailContainer.montarEmail("[EMERGÊNCIA] Falta de Gás", ambiente.get("vraptor.simplemail.main.from"), conteudo));
+			Email email = this.emailContainer.montarEmail(assunto,
+					destinatario, conteudo);
+			if (comCopia != null) {
+				email.addCc(comCopia);
+			}
+			this.emailContainer.enviarEmail(email);
+			logarEnvio(assunto, destinatario, comCopia, conteudo);
+			result.use(Results.json()).from("ok", "Sucesso no envio do e-mail");
+
+		} catch (EmailException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result.use(Results.http()).sendError(500,
+					"Falha no envio do e-mail: " + e.getMessage());
+		}
+	}
+
+	@Post("/emergencia-falta-gas")
+	public void enviarEmail(String emailCliente, String conteudo) {
+		try {
+			String destinatario = ambiente.get("vraptor.simplemail.main.from");
+			Email email = this.emailContainer.montarEmail(
+					Assuntos.EMERGENCIA_FALTA_GAS, destinatario, conteudo);
+			email.addCc(emailCliente);
+			this.emailContainer.enviarEmail(email);
+			logarEnvio(Assuntos.EMERGENCIA_FALTA_GAS, destinatario,
+					emailCliente, conteudo);
 			result.use(Results.json()).from("ok", "Sucesso no envio do e-mail");
 		} catch (EmailException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}
 	}
 
-	
+	private void logarEnvio(String assunto, String destinatario,
+			String comCopia, String conteudo) {
+		log.info("Sucesso no envio do e-mail para o destinatário: "
+				+ destinatario);
+		log.info("--- DETALHES ---");
+		log.info("Assunto: " + assunto);
+		log.info("Cópia: " + comCopia);
+		log.info("Contéudo: " + conteudo);
+	}
 
 }
---
+++ src/main/java/br/com/logique/controller/email/Assuntos.java
@@ -0,0 +1,7 @@
+package br.com.logique.controller.email;
+
+public class Assuntos {
+	
+	public static final String EMERGENCIA_FALTA_GAS = "[EMERGÊNCIA] Falta de Gás";
+
+}
